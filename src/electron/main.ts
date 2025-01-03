39174f77001879246270bebbd264c8995d3b62b7
alexkolitenkov <alex.kolitenkov@gmail.com>
2025-01-04 00:58:19 +0200

    feat: finished scaffolding project

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,15 +1,17 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
+import { isDev } from './util.js';
+import { getPreloadPath } from './pathResolever.js';
 
 app.on('ready', () => {
     const mainWindow = new BrowserWindow({
-        // width: 800,
-        // height: 600,
-        // webPreferences: {
-        //     preload: path.join(__dirname, 'preload.js'),
-        // },
+        webPreferences: {
+            preload: getPreloadPath()
+        }
     });
-
-    mainWindow.loadFile(path.join(app.getAppPath(), 'dist-react/index.html'));
+    if (isDev()) {
+        mainWindow.loadURL('http://localhost:5123');
+    } else {
+        mainWindow.loadFile(path.join(app.getAppPath(), 'dist-react/index.html'));
+    }
 });
-
